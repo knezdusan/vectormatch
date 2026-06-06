5188cfd954aa0c1305e0e8b868d47520bdc0438b
Dusan Knezevic <knez@Dusans-MacBook-Air.local>
2026-06-06 20:15:56 +0200

    Better Auth minor changes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,20 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 import { db } from "@/db/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
+  plugins: [nextCookies()],
+  emailAndPassword: {
+    enabled: true,
+  },
 });
